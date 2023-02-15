5a9226e25e31a113cf34ff22bda7ac39d3a68076
vadim l <vadimlozinschii@gmail.com>
2023-02-15 09:10:38 -0800

    wrote 4 passing functions

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your solution in this file!
+const employee = {
+    employee1: {
+        name: "Zaddy",
+        streetAddress: "1738 St Nw, Wop City, North Dakota"
+    }
+
+}
+
+function updateEmployeeWithKeyAndValue(employee, name, streetAddress) {
+    const cloneEmployee = {...employee};
+    cloneEmployee[name] = streetAddress;
+    return cloneEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, name, streetAddress) {
+    employee[name] = streetAddress;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, name, streetAddress) {
+    const cloneEmployee = {...employee};
+    cloneEmployee[name] = streetAddress;
+    return cloneEmployee;
+}
+    const newEmployee = deleteFromEmployeeByKey(employee);
+
+function destructivelyDeleteFromEmployeeByKey(employee, name, streetAddress) {
+    employee.name = name;
+    employee[name] = streetAddress;
+    return employee;
+}
